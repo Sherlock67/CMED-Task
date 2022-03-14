342d56a5a4eeb33921d64abd55e4d89b60140450
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-15 05:08:23 +0600

    service layer added

---
+++ CMED-BusinessLayer/Service/PrescriptionService.cs
@@ -0,0 +1,70 @@
+﻿using CMED_DataAcessLayer.Interface;
+using CMED_DataAcessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMED_BusinessLayer.Service
+{
+    public class PrescriptionService
+    {
+        private readonly IPrescription prescription;
+        public PrescriptionService(IPrescription _prescription)
+        {
+            prescription = _prescription;
+        }
+        public IEnumerable<Prescription> GetPrescriptions(int id)
+        {
+            return prescription.GetAll().Where(x => x.Prescription_Id == id).ToList();
+        }
+        public IEnumerable<Prescription> GetAllPrescription()
+        {
+            try
+            {
+                return prescription.GetAll().ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<Prescription> AddNewPrescription(Prescription p)
+        {
+            return await prescription.Create(p);
+        }
+        public async Task DeletePrescription(int id)
+        {
+
+            try
+            {
+                var DataList = prescription.GetById(id);
+                prescription.Delete(DataList);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        public async Task UpdatePrescription(int id)
+        {
+            try
+            {
+                var DataList = prescription.GetById(id);
+                prescription.Update(DataList);
+                //foreach (var item in DataList)
+                //{
+                //    prescription.Update(item);
+                //}
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ CMED-BusinessLayer/Service/RegistrationService.cs
@@ -0,0 +1,37 @@
+﻿using CMED_DataAcessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMED_BusinessLayer.Service
+{
+    public class RegistrationService
+    {
+        private readonly IRegistration _reg;
+        public RegistrationService(IRegistration reg)
+        {
+            _reg = reg;
+
+        }
+        public async Task<Registration> AddUserAsync(Registration r)
+        {
+            return await _reg.Create(r);
+        }
+        public async Task<bool> VerifyUserAsync(Login login)
+        {
+            var obj = await _reg.GetUserByEmailId(login.EmailId);
+            if (obj != null)
+            {
+                if (obj.Password == login.Password)
+                {
+                    return true;
+
+                }
+            }
+            return false;
+
+        }
+    }
+}
