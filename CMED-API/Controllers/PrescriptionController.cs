0b0a1833713f926bd8b58071047de6392fa76d39
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-15 05:18:04 +0600

    working on prescription api

---
+++ CMED-API/Controllers/PrescriptionController.cs
@@ -0,0 +1,69 @@
+﻿using CMED_BusinessLayer.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMED_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrescriptionController : ControllerBase
+    {
+        private readonly PrescriptionService _prescription;
+        public PrescriptionController(PrescriptionService prescription)
+        {
+            _prescription = prescription;
+
+        }
+        [HttpPost("CreatePrescription")]
+        public async Task<Object> CreatePrescription([FromBody] Prescription prescription)
+        {
+            try
+            {
+                await _prescription.AddNewPrescription(prescription);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        [HttpGet("GetAllPrescription")]
+        public Object GetAllPrescription()
+        {
+            var data = _prescription.GetAllPrescription();
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                }
+            );
+            return json;
+        }
+        [HttpDelete("DeletePerson")]
+        public bool DeletePrescription(int id)
+        {
+            try
+            {
+                _prescription.DeletePrescription(id);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        [HttpPut("UpdatePrescription")]
+        public bool UpdatePerson(int id)
+        {
+            try
+            {
+                _prescription.UpdatePrescription(id);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
