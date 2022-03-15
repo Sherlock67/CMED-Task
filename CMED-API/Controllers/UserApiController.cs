b3d2289cb431a0a057a805b5db97313f99ba88ac
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-16 01:31:22 +0600

    adding user on progress

---
+++ CMED-API/Controllers/UserApiController.cs
@@ -0,0 +1,33 @@
+﻿using CMED_BusinessLayer.Service;
+using CMED_DataAcessLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMED_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        private readonly RegistrationService _registration;
+        public UserApiController(RegistrationService registration)
+        {
+            _registration = registration;
+
+        }
+        [HttpPost("CreateUser")]
+        public async Task<Object> CreateUser([FromBody] Registration registration)
+        {
+            try
+            {
+                await _registration.AddUserAsync(registration);
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CMED-WEB/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using CMED_DataAcessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace CMED_WEB.Controllers
+{
+    public class UserController : Controller
+    {
+        private static string url = "https://localhost:7075/";
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser(Registration registration)
+        {
+            string custommsg = string.Empty;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseMsg = await client.PostAsJsonAsync("/api/Prescription/CreateUser", registration);
+                if (responseMsg != null)
+                {
+                    var res = responseMsg.Content.ReadAsStringAsync().Result;
+                    custommsg = JsonConvert.DeserializeObject<string>(res);
+                }
+            }
+            return View();
+        }
+    }
+}
