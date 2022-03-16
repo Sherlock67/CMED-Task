0ef5eb6459ce7cc06563e44f563f489e0d9d9743
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-16 20:01:15 +0600

    Finalizing things and final touch

--- CMED-WEB/Controllers/MedicalServiceController.cs
+++ CMED-WEB/Controllers/MedicalServiceController.cs
@@ -8,18 +8,18 @@ namespace CMED_WEB.Controllers
     public class MedicalServiceController : Controller
     {
         public async Task<IActionResult> Index()
+        
         {
-            List<Root> Service = new List<Root>();
+            Root  Service = new Root();
             using (var httpClient = new HttpClient())
             {
                 using (var response = await httpClient.GetAsync("https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=341248"))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
-                    Service = JsonConvert.DeserializeObject<List<Root>>(apiResponse);
+                    Service = JsonConvert.DeserializeObject<Root>(apiResponse);
                 }
             }
             return View(Service);
         }
-
     }
 }
