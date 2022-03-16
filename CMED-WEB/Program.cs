7bd29b76700434b5cce39f9473932c56515935bd
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-16 18:14:34 +0600

    finalizing things

--- CMED-WEB/Program.cs
+++ CMED-WEB/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=AddUser}/{id?}");
 
 app.Run();
